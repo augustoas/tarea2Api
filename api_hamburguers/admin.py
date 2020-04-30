ebfc41062af9c3217de8d093b49ab42c6535bbd3
Augusto <aaalvarez@uc.cl>
2020-04-30 17:51:19 -0400

    asdf

--- api_hamburguers/admin.py
+++ api_hamburguers/admin.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
-from api_hamburguers.models import Hamburguesa, Ingrediente
 
 # Register your models here.
 
-admin.site.register(Hamburguesa)
-admin.site.register(Ingrediente)
+
