e00e825579526c6d4b73590a76527ae5a8682dd2
Augusto <aaalvarez@uc.cl>
2020-05-03 00:14:35 -0400

    asdf

--- api_hamburguers/urls.py
+++ api_hamburguers/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import hamburguer_list, hamburguer_detail, ingredient_list, ingredient_detail, hamburguer_ingredient
 
 urlpatterns = [
-    path('hamburguesa', hamburguer_list),
+    path('hamburguesa/', hamburguer_list),
     path('hamburguesa/<pk>/', hamburguer_detail),
-    path('ingrediente', ingredient_list),
+    path('ingrediente/', ingredient_list),
     path('ingrediente/<pk>/', ingredient_detail),
     path('hamburguesa/<pk_h>/ingrediente/<pk_i>/',hamburguer_ingredient)  
 ]
