30ea6390ffd4cc10a2d159b617e55d6f13fcdbb2
Augusto <aaalvarez@uc.cl>
2020-05-03 00:34:59 -0400

    asdf

--- api_hamburguers/urls.py
+++ api_hamburguers/urls.py
@@ -4,8 +4,13 @@ from .views import hamburguer_list, hamburguer_detail, ingredient_list, ingredie
 
 urlpatterns = [
     path('hamburguesa/', hamburguer_list),
+    path('hamburguesa', hamburguer_list),
     path('hamburguesa/<pk>/', hamburguer_detail),
+    path('hamburguesa/<pk>', hamburguer_detail),
     path('ingrediente/', ingredient_list),
+    path('ingrediente', ingredient_list),
     path('ingrediente/<pk>/', ingredient_detail),
+    path('ingrediente/<pk>', ingredient_detail),
     path('hamburguesa/<pk_h>/ingrediente/<pk_i>/',hamburguer_ingredient)  
+    path('hamburguesa/<pk_h>/ingrediente/<pk_i>',hamburguer_ingredient)
 ]
--- api_hamburguers/views.py
+++ api_hamburguers/views.py
@@ -16,29 +16,29 @@ def hamburguer_list(request):
             for index_2,i in enumerate(h['ingredientes']):                
                 serializer.data[index]['ingredientes'][index_2] = {'path': f"https://herokudjangoapihamburguer.herokuapp.com/ingrediente/{i}"}
             
-        return Response({'message': 'Hamburguesas', 'body': serializer.data}, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'POST': #OK
         serializer = HamburguesaSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'Hamburguesa creada ', 'body': serializer.data}, status=status.HTTP_201_CREATED)
-        return Response({'message': 'Input invalido ', 'body': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PATCH', 'DELETE'])
 def hamburguer_detail(request,pk):
     
     aux = pk.isdigit()
     if aux == False:
-        return Response({'message': 'id invalido '}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
     try:
         hamburguer = Hamburguesa.objects.get(pk=pk)
     
     except:
 
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET': #OK
         
@@ -47,7 +47,7 @@ def hamburguer_detail(request,pk):
         for index,i in enumerate(serializer.data['ingredientes']):                
             serializer.data['ingredientes'][index] = {'path': f"https://herokudjangoapihamburguer.herokuapp.com/ingrediente/{i}"}
         
-        return Response({'message': 'Operacion exitosa ', 'body': serializer.data}, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'PATCH': #OK (REVISAR SI EL ID FUE CAMBIADO O NO)
         
@@ -58,12 +58,12 @@ def hamburguer_detail(request,pk):
             for index,i in enumerate(serializer.data['ingredientes']):                
                 serializer.data['ingredientes'][index] = {'path': f"https://herokudjangoapihamburguer.herokuapp.com/ingrediente/{i}"}
 
-            return Response({'message': 'Operacion exitosa ', 'body': serializer.data}, status=status.HTTP_200_OK)
-        return Response({'message': 'Parámetros invalidos ', 'body': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE': #OK
         hamburguer.delete()
-        return Response({'message: Hamburguesa eliminada'},status = status.HTTP_200_OK)
+        return Response(serializer.errors,status = status.HTTP_200_OK)
 
 
 @api_view(['GET', 'POST'])
@@ -71,15 +71,15 @@ def ingredient_list(request): #OK
     if request.method == 'GET': #OK
         ingredients = Ingrediente.objects.all()
         serializer = IngredienteSerializer(ingredients, many=True)
-        return Response({'message': 'Operacion exitosa ', 'body': serializer.data}, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'POST': #OK
         serializer = IngredienteSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'Ingrediente creado ', 'body': serializer.data}, status=status.HTTP_201_CREATED)
-        return Response({'message': 'Input invalido ', 'body': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','DELETE'])
 def ingredient_detail(request,pk): #OK
@@ -89,19 +89,19 @@ def ingredient_detail(request,pk): #OK
     
     except:
 
-        return Response({'message': 'Id invalido '}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET': #OK
         serializer = IngredienteSerializer(ingredient)
-        return Response({'message': 'Operacion exitosa ', 'body': serializer.data}, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'DELETE': #OK
 
         if Hamburguesa.objects.filter(ingredientes=ingredient).exists():
-            return Response({'message: Ingrediente no se puede eliminar, se encuentra en una hamburguesa'},status = status.HTTP_409_CONFLICT)
+            return Response(serializer.errors, status = status.HTTP_409_CONFLICT)
         else:
             ingredient.delete()
-            return Response({'message: Ingrediente eliminado'},status = status.HTTP_200_OK)
+            return Response(serializer.errors, status = status.HTTP_200_OK)
         
 
 @api_view(['GET','PUT', 'DELETE'])
@@ -111,13 +111,13 @@ def hamburguer_ingredient(request,pk_h,pk_i):
         hamburguer = Hamburguesa.objects.get(pk=pk_h)
     
     except:
-        return Response({'message': 'Hamburguesa no valida '},status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
     try:
         ingredient = Ingrediente.objects.get(pk=pk_i)
     
     except:
-        return Response({'message': 'Ingrediente no existe '},status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET': #OK
         ingredients = Ingrediente.objects.filter(pk=pk_i)
@@ -130,7 +130,7 @@ def hamburguer_ingredient(request,pk_h,pk_i):
                 serializer_h.data[index]['ingredientes'][index_2] = {'path': f"https://herokudjangoapihamburguer.herokuapp.com/ingrediente/{i}"}
        
 
-        return Response({'message': 'Operacion exitosa ', 'body': {'hamburguesa': serializer_h.data, 'ingrediente': serializer_i.data}}, status=status.HTTP_200_OK)
+        return Response(serializer_h.data, status=status.HTTP_200_OK)
 
     if request.method == 'PUT': #OK
         hamburguer.ingredientes.add(ingredient)
@@ -143,7 +143,7 @@ def hamburguer_ingredient(request,pk_h,pk_i):
             for index_2,i in enumerate(h['ingredientes']):                
                 serializer_h.data[index]['ingredientes'][index_2] = {'path': f"https://herokudjangoapihamburguer.herokuapp.com/ingrediente/{i}"}
             
-        return Response({'message': 'Ingrediente agregado', 'body': {'hamburguesa': serializer_h.data, 'ingrediente': serializer_i.data}}, status=status.HTTP_201_CREATED)
+        return Response(serializer_h.data, status=status.HTTP_201_CREATED)
 
     elif request.method == 'DELETE': #OK
 
@@ -157,4 +157,4 @@ def hamburguer_ingredient(request,pk_h,pk_i):
             for index_2,i in enumerate(h['ingredientes']):                
                 serializer_h.data[index]['ingredientes'][index_2] = {'path': f"https://herokudjangoapihamburguer.herokuapp.com/ingrediente/{i}"}
 
-        return Response({'message': 'Ingrediente retirado', 'body': {'hamburguesa': serializer_h.data, 'ingrediente': serializer_i.data}}, status=status.HTTP_200_OK)
+        return Response(serializer_h.data, status=status.HTTP_200_OK)
